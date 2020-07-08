d66d7b71788095a119f8274eed1053de2f8fa145
Kyle Rudderforth <kruddzz@gmail.com>
2020-07-07 21:48:02 -0500

    .gitignore added

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -2,7 +2,7 @@ const Employee = require("./employee")
 
 class Intern extends Employee {
     constructor(name, id, email, school) {
-        super(name,id, email);
+        super(name, id, email);
         this.school = school;
     }
 
