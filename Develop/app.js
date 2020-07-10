4011d58ebdf0974e89ddf2d55cf019054abc7188
Kyle Rudderforth <kruddzz@gmail.com>
2020-07-09 20:46:33 -0500

    validation added for manager qustions

--- Develop/app.js
+++ Develop/app.js
@@ -12,46 +12,79 @@ const render = require("./lib/htmlRenderer");
 
 // Variables
 const teamMembers = [];
+const idArray = [];
+const emailArray = [];
+const githubArray = [];
+
 
 function teamBuilder() {
   // create a function that produces manager profile
     // the function uses inquirer to ask manager questions
     // then goes to a creatTeam() function to inquire on what type of team members to add
     function createManager() {
-      console.log("Build your Team!!");
+      console.log("Build your team!");
       inquirer.prompt([
         {
           type: "input",
           name: "managerName",
           message: "What is your manager's name?",
+          validate: answer => {
+            if (answer !== "") {
+              return true;
+            }
+            return "Please enter at least one character.";
+          }
         },
-
         {
           type: "input",
           name: "managerId",
           message: "What is your manager's id?",
-          
+          validate: answer => {
+            const pass = answer.match(
+              /^[1-9]\d*$/
+            );
+            if (pass) {
+              return true;
+            }
+            return "You have to enter positive number greater than zero!";
+          }
         },
-
         {
           type: "input",
           name: "managerEmail",
           message: "What is your manager's email?",
+          validate: answer => {
+            const pass = answer.match(
+              /\S+@\S+\.\S+/
+            );
+            if (pass) {
+              return true;
+            }
+            return "Enter a valid email address.";
+          }
         },
-
         {
           type: "input",
           name: "managerOfficeNumber",
           message: "What is your manager's office number?",
-         
+          validate: answer => {
+            const pass = answer.match(
+              /^[1-9]\d*$/
+            );
+            if (pass) {
+              return true;
+            }
+            return "You have to enter a positive number greater than zero!";
+          }
         }
       ]).then(answers => {
         const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
-        // pushes manager info to teamMembers array
         teamMembers.push(manager);
-        // calls a function to create a team
+        idArray.push(answers.managerId);
+        emailArray.push(answers.managerEmail);
         createTeam();
       });
+  
     }
 
     // create a function to create rest of team
@@ -172,25 +205,3 @@ function teamBuilder() {
 
 // initialize program
 teamBuilder ();
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
