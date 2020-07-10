171fcf52f37cce7a1653d403800453ced740a164
Kyle Rudderforth <kruddzz@gmail.com>
2020-07-09 21:19:13 -0500

    valafation for intern questions

--- Develop/app.js
+++ Develop/app.js
@@ -202,33 +202,68 @@ function teamBuilder() {
               type: "input",
               name: "internName",
               message: "What is your intern's name?",
+              validate: answer => {
+                if (answer !== "") {
+                  return true;
+                }
+                return "Enter at least one character!";
+              }
             },
-
             {
               type: "input",
               name: "internId",
               message: "What is your intern's id?",
+              validate: answer => {
+                const pass = answer.match(
+                  /^[1-9]\d*$/
+                );
+                if (pass) {
+                  if (idArray.includes(answer)) {
+                    return "ID already taken. Enter a different number!";
+                  } else {
+                    return true;
+                  }
+                              
+                }
+                return "You have to enter a positive number greater than zero.";
+              }
             },
-            
             {
               type: "input",
               name: "internEmail",
               message: "What is your intern's email?",
-              
+              validate: answer => {
+                const pass = answer.match(
+                  /\S+@\S+\.\S+/
+                );
+                if (pass) { 
+                  if (emailArray.includes(answer)) {
+                  return "Email already used. Please enter a different email!";
+                } else {
+                  return true;
+                }
+              }
+                return "Please enter a valid email address!";
+              }
             },
-
             {
               type: "input",
               name: "internSchool",
               message: "What is your intern's school?",
+              validate: answer => {
+                if (answer !== "") {
+                  return true;
+                }
+                return "Please enter at least one character.";
+              }
             }
-            
           ]).then(answers => {
             const intern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.internSchool);
-            // push the intern info to the teamMembers array
             teamMembers.push(intern);
+            idArray.push(answers.internId);
             createTeam();
           });
+      
         }
 
       // create a buildTeam(function)
